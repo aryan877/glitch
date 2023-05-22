f63e4590747d8d70698678d41677bf3da83a44c7
Aryan Kumar <aryankumar@Aryans-MacBook-Pro.local>
2023-05-23 03:29:20 +0530

    env for public id

--- utils/appwriteConfig.ts
+++ utils/appwriteConfig.ts
@@ -4,4 +4,4 @@ export const client = new Client();
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('646974292da1247c582a');
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_ID as string);
